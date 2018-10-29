3a33a7fddd094d5003d1a09aaabcda7d505e98f2
Robin Naundorf <r.naundorf@fh-muenster.de>
2018-10-29 09:22:50 +0100

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Robin Naundorf on 23.10.18.
+//  Copyright © 2018 Robin Naundorf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+ 
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
+}
